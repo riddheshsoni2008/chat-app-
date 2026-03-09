cc75df3e992e75dbe8e228971e1858aa7b290513
riddheshsoni <ns3401976@gmail.com>
2026-03-09 17:47:00 +0530

    fix socket connection

--- server/client/src/App.jsx
+++ server/client/src/App.jsx
@@ -1,10 +1,7 @@
 import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("https://chat-app-olcd.onrender.com", {
-  transports: ["websocket"],
-});
-
+const socket = io("https://chat-app-olcd.onrender.com");
 function App() {
   const [username, setUsername] = useState("");
   const [nameInput, setNameInput] = useState("");
