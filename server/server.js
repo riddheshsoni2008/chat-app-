ff2d1072905810a5431db6d3da7b5048e66f9b0c
riddheshsoni <ns3401976@gmail.com>
2026-03-09 15:50:48 +0530

    chat app

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+  socket.on("join_room", (room) => {
+    socket.join(room);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
