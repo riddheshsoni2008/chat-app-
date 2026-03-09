4888d90bb3c39bf1e508d5c323e7d0298d108930
riddheshsoni <ns3401976@gmail.com>
2026-03-09 18:29:36 +0530

    server fix

--- server/client/src/App.jsx
+++ server/client/src/App.jsx
@@ -11,17 +11,17 @@ function App() {
 
   // socket connect
   io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+    console.log("User connected:", socket.id);
 
-  socket.on("join_room", (room) => {
-    socket.join(room);
-  });
+    socket.on("join_room", (room) => {
+      socket.join(room);
+    });
 
-  socket.on("send_message", (data) => {
-    console.log("message:", data);
-    socket.to(data.room).emit("receive_message", data);
+    socket.on("send_message", (data) => {
+      console.log("message:", data);
+      socket.to(data.room).emit("receive_message", data);
+    });
   });
-});
 
   const joinChat = () => {
     if (nameInput !== "" && socket) {
--- server/server.js
+++ server/server.js
@@ -6,6 +6,11 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+// simple route to check server
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
@@ -13,38 +18,34 @@ const io = new Server(server, {
     origin: "*",
     methods: ["GET", "POST"],
   },
-  pingTimeout: 60000,
-  pingInterval: 25000,
 });
 
+// socket connection
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
+  // join room
   socket.on("join_room", (room) => {
     socket.join(room);
+    console.log(`User joined room: ${room}`);
   });
 
+  // receive message
   socket.on("send_message", (data) => {
     console.log("message:", data);
-    socket.to(data.room).emit("receive_message", data);
+
+    // send message to room
+    io.to(data.room).emit("receive_message", data);
+  });
+
+  // disconnect
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
   });
 });
 
 const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
-
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
-
-  socket.on("join_room", (room) => {
-    socket.join(room);
-  });
-
-  socket.on("send_message", (data) => {
-    console.log("message:", data);
-    socket.to(data.room).emit("receive_message", data);
-  });
+  console.log(`Server running on port ${PORT}`);
 });
